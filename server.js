bf1b9c804fef101afd92d4b8a23663599265cf0c
向阳 <suiyuran@163.com>
2018-01-09 15:41:07 +0800

    add init module

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const cluster = require('cluster');
+const init = require('./server/module/init');
 const logger = require('./server/module/logger');
 const workerNameList = ['master', 'agentd', 'job', 'task'];
 const getWorkerNameFromConf = id => ({ name: workerNameList[id] });
 const getWorkerNameFromProc = worker => (worker.process.env.name);
 const startNewWorker = id => {
-	cluster.fork(getWorkerNameFromConf(id));
-	cluster.workers[id].on('message', messageHandler);
-	cluster.workers[id].on('exit', cluster.fork.bind(this, getWorkerNameFromConf(id)));
+	let { init } = cluster.settings;
+	if (id !== 1 && !init) {
+		logger.info('system not init, no more worker need to run');
+	} else {
+		cluster.fork(getWorkerNameFromConf(id));
+		cluster.workers[id].on('message', messageHandler);
+		cluster.workers[id].on('exit', cluster.fork.bind(this, getWorkerNameFromConf(id)));
+	}
 };
 const messageHandler = (msg) => {
 	switch (msg) {
@@ -24,11 +30,13 @@ const messageHandler = (msg) => {
 	}
 };
 if (cluster.isMaster) {
+	let initStatus = init.checkInitStatus();
+	cluster.settings.init = initStatus;
 	logger.info('master ready');
 	startNewWorker(1);
 } else {
-	let workerName = getWorkerNameFromProc(cluster.worker);
-	switch (workerName) {
+	let workName = getWorkerNameFromProc(cluster.worker);
+	switch (workName) {
 		case 'agentd':
 			require('./server/agentd/index');
 			logger.info('agentd ready');
@@ -45,6 +53,6 @@ if (cluster.isMaster) {
 			process.send('task ready');
 			break;
 		default:
-			logger.info('no worker need to run');
+			logger.info('no more worker need to run');
 	}
 }
