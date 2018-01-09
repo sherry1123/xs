cff9e82233fdb2efb7120968e4dbdb7fe7a8a60b
向阳 <suiyuran@163.com>
2018-01-09 13:58:25 +0800

    separate controller & service

--- server/controller/test.js
+++ server/controller/test.js
@@ -1,15 +1,15 @@
-const testService = require('../service/test');
+const service = require('../service');
 const model = {
 	index(ctx) {
 		ctx.body = 'Home Page';
 	},
 	async get(ctx) {
-		let result = await testService.get();
+		let result = await service.test.get();
 		ctx.body = result;
 	},
 	async post(ctx) {
 		let param = ctx.param;
-		let result = await testService.post(param);
+		let result = await service.test.post(param);
 		ctx.body = result;
 	}
 }
