28646e549e17da77b198854ea011ef8fd286c4d7
向阳 <suiyuran@163.com>
2018-01-11 10:32:34 +0800

    modify model

--- server/model/test.js
+++ server/model/test.js
@@ -1,7 +1,5 @@
 const mongoose = require('../config/database');
-const Schema = mongoose.Schema;
-const test = new Schema({
+module.exports = mongoose.model('test', new mongoose.Schema({
     id: Number,
     text: String
-});
-module.exports = mongoose.model('test', test);
+}));
