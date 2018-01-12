995d879a7a0a25c0c2c07613af72eb3352bead46
向阳 <suiyuran@163.com>
2018-01-12 15:07:26 +0800

    add output log into file

--- server/module/logger.js
+++ server/module/logger.js
@@ -1,11 +1,20 @@
 const log4js = require('log4js');
+const config = require('../config');
 
 log4js.configure({
 	appenders: {
-		out: { type: 'console' }
+		console: { 
+			type: 'console'
+		},
+		file: {
+			type: 'file',
+			filename: `${config.env.root}/log/log4js.log`,
+			maxLogSize: 1024 * 1024 * 10,
+			backups: 3
+		}
 	},
 	categories: {
-		default: { appenders: ['out'], level: 'info' }
+		default: { appenders: ['console', 'file'], level: 'info' },
 	},
 	disableClustering: true
 });
