a7803521cb09855b3b3809a83f9374290e832929
向阳 <suiyuran@163.com>
2018-01-12 10:45:19 +0800

    modify user model

--- server/model/user.js
+++ server/model/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('../config/database');
-module.exports = mongoose.model('user', new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    firstname: String,
-    lastname: String,
-    group: String,
-    type: String,
-    receivemail: Boolean,
-    useravatar: Number
-}));
+module.exports = mongoose.model('user',
+    new mongoose.Schema({
+        username: String,
+        password: String,
+        email: String,
+        firstname: String,
+        lastname: String,
+        group: String,
+        type: String,
+        receivemail: Boolean,
+        useravatar: Number
+    })
+);
