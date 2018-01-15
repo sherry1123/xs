eee51767750f6e9dcdfffdeebf8dc73a51ba5bef
向阳 <suiyuran@163.com>
2018-01-15 16:09:06 +0800

    modify get memory usage

--- server/agentd/service.js
+++ server/agentd/service.js
@@ -4,39 +4,32 @@ const getCpuInfo = () => {
     let cpus = os.cpus();
     let current = ['user', 'sys'];
     let total = 0;
+    let used = 0;
     let usage = 0;
     let core = cpus.length;
     cpus.forEach(cpu => {
         let { times } = cpu;
         for (let i of Object.keys(times)) {
             total += times[i];
-            usage += current.includes(i) ? times[i] : 0;
+            used += current.includes(i) ? times[i] : 0;
         }
     });
-    return { core, total, usage };
+    return { core, total, used, usage };
 };
-const getMemoryInfo = () => {
+let CPU = getCpuInfo();
+new CronJob('*/15 * * * * *', () => {
+    let cpu = getCpuInfo();
+    let usage = (cpu.used - CPU.used) / (cpu.total - CPU.total) * 100;
+    CPU.total = cpu.total;
+    CPU.used = cpu.used;
+    CPU.usage = usage;
+}, null, true);
+exports.getCpuUsage = () => {
+    return CPU;
+};
+exports.getMemoryUsage = () => {
     let total = os.totalmem();
     let free = os.freemem();
     let usage = (1 - free / total) * 100;
     return { total, free, usage };
-};
-let CPU = getCpuInfo();
-let _core = CPU.core;
-let _total = CPU.total;
-let _usage = CPU.usage;
-let CPUUSAGE = 0;
-new CronJob('*/15 * * * * *', () => {
-    let cpu = getCpuInfo();
-    let used = (cpu.usage - _usage) / (cpu.total - _total);
-    _total = cpu.total;
-    _usage = cpu.usage;
-    CPUUSAGE = used * 100;
-}, null, true);
-let MEMORY = getMemoryInfo();
-exports.getCpuUsage = () => {
-    return { core: _core, usage: CPUUSAGE }
-};
-exports.getMemoryUsage = () => {
-    return MEMORY;
 };
