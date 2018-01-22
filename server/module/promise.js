e2681938c714af03797ce85076b220a6dd32877f
向阳 <suiyuran@163.com>
2018-01-22 12:46:06 +0800

    modify module

--- server/module/promise.js
+++ server/module/promise.js
@@ -1,27 +1,23 @@
 const fs = require('fs');
 const child = require('child_process');
-const model = {
-  runCommandInPromise(command) {
-    return new Promise((resolve, reject) => {
-      child.exec(command, (error, stdout, stderr) => {
-        error ? reject(stderr) : resolve(stdout);
-      });
+exports.runCommandInPromise = command => {
+  return new Promise((resolve, reject) => {
+    child.exec(command, (error, stdout, stderr) => {
+      error ? reject(stderr) : resolve(stdout);
     });
-  },
-  readFileInPromise(path) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(path, 'utf8', (error, data) => {
-        error ? reject(error) : resolve(data);
-      });
+  });
+};
+exports.readFileInPromise = path => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      error ? reject(error) : resolve(data);
     });
-  },
-  writeFileInPromise(path, data) {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(path, data, 'utf8', error => {
-        error ? reject(error) : resolve();
-      });
+  });
+};
+exports.writeFileInPromise = (path, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, 'utf8', error => {
+      error ? reject(error) : resolve();
     });
-  }
-}
-
-module.exports = model;
+  });
+};
