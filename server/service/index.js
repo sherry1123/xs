a78e08026e3461edba3a56b704953246ea737e41
向阳 <suiyuran@163.com>
2018-01-25 20:59:16 +0800

    add some code in init cluster

--- server/service/index.js
+++ server/service/index.js
@@ -198,6 +198,23 @@ const model = {
         let {iplist} = param;
         try {
             //await init.initMongoDB(iplist);
+            // let token = await request.get(config.api.orcafs.gettoken);
+            // let result = await request.post(config.api.orcafs.createcluster, param, token);
+            // if (result.errorid === 103) {
+            //     errorHandler(18, result.message, param);
+            // } else {
+            //     let data = {};
+            //     let getInitStatus = setInterval(() => {
+            //         data = await request.get(config.api.orcafs.installstatus, null, token);
+            //         if (data.errorid === 103) {
+            //             errorHandler(18, data.message, param);
+            //             clearInterval(getInitStatus);
+            //             await model.antiInitCluster();
+            //         } else {
+            //             socket.postInitStatus(data);
+            //         }
+            //     }, 1000);
+            // }
         } catch (error) {
             errorHandler(18, error, param);
         }
