2d8b830f695f06552f2a75b6ed4b667b9cb416ee
向阳 <suiyuran@163.com>
2018-02-25 10:34:30 +0800

    rename function

--- server/module/promise.js
+++ server/module/promise.js
@@ -7,6 +7,14 @@ exports.runCommandInPromise = command => {
     });
   });
 };
+exports.runCommandInRemoteNodeInPromise = (ip, command) => {
+  command = `ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no denali@${ip} -i /home/denali/denali/insecure_private_key '${command}'`;
+  return new Promise((resolve, reject) => {
+    child.exec(command, (error, stdout, stderr) => {
+      error ? reject(stderr) : resolve(stdout);
+    });
+  });
+};
 exports.readFileInPromise = path => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (error, data) => {
@@ -18,14 +26,6 @@ exports.writeFileInPromise = (path, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(path, data, 'utf8', error => {
       error ? reject(error) : resolve();
-    });
-  });
-};
-exports.runCommandInRemoteNode = (ip, command) => {
-  command = `ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no denali@${ip} -i /home/denali/denali/insecure_private_key '${command}'`;
-  return new Promise((resolve, reject) => {
-    child.exec(command, (error, stdout, stderr) => {
-      error ? reject(stderr) : resolve(stdout);
     });
   });
 };
