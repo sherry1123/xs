14eb28a07f8a96d11e06781f547fddf79cbc49ff
向阳 <suiyuran@163.com>
2018-02-26 13:34:23 +0800

    add chmod file

--- server/module/promise.js
+++ server/module/promise.js
@@ -7,14 +7,6 @@ exports.runCommandInPromise = command => {
     });
   });
 };
-exports.runCommandInRemoteNodeInPromise = (ip, command) => {
-  command = `ssh -i /home/denali/denali/insecure_private_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet denali@${ip} "${command}"`;
-  return new Promise((resolve, reject) => {
-    child.exec(command, (error, stdout, stderr) => {
-      error ? reject(stderr) : resolve(stdout);
-    });
-  });
-};
 exports.readFileInPromise = path => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (error, data) => {
@@ -33,6 +25,21 @@ exports.copyFileInPromise = (src, dest) => {
   return new Promise((resolve, reject) => {
     fs.copyFile(src, dest, error => {
       error ? reject(error) : resolve();
+    });
+  });
+};
+exports.chmodFileInPromise = (path, mode) => {
+  return new Promise((resolve, reject) => {
+    child.exec(`sudo chmod ${mode} ${path}`, (error, stdout, stderr) => {
+      error ? reject(stderr) : resolve(stdout);
+    });
+  });
+}
+exports.runCommandInRemoteNodeInPromise = (ip, command) => {
+  command = `ssh -i /home/denali/denali/insecure_private_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet denali@${ip} "${command}"`;
+  return new Promise((resolve, reject) => {
+    child.exec(command, (error, stdout, stderr) => {
+      error ? reject(stderr) : resolve(stdout);
     });
   });
 };
