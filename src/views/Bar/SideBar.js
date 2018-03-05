8282c60ea87d747b62d3ab677f1d4c3b71b9ead1
liuxiaoyu <461565610@qq.com>
2018-03-05 20:38:52 +0800

    side bar

--- src/views/Bar/SideBar.js
+++ src/views/Bar/SideBar.js
@@ -14,7 +14,7 @@ class SideBar extends Component {
         this.getSubMenuByPath(key);
         this.state = {
             direction: 'down',
-            menuVisible: true
+            menuExpand: true
         };
     }
 
@@ -30,9 +30,9 @@ class SideBar extends Component {
         window.onhashchange = null;
     }
 
-    changeMenuVisible (){
-        let menuVisible = !this.state.menuVisible;
-        this.setState({menuVisible});
+    changeMenuExpand (){
+        let menuExpand = !this.state.menuExpand;
+        this.setState({menuExpand});
     }
 
     switchScrollDirection (direction){
@@ -63,35 +63,55 @@ class SideBar extends Component {
 
     render (){
         return (
-            <aside className={`fs-sidebar-wrapper ${this.state.direction}  ${this.state.menuVisible ? '' : 'hide'}`}>
+            <aside className={`fs-sidebar-wrapper ${this.state.direction}  ${this.state.menuExpand ? '' : 'hide'}`}>
                 <div className={`fs-visible-operation-wrapper`}>
-                    <Button className={`fs-visible-operation-button ${this.state.menuVisible ? '' : 'rock'}`} shape="circle" icon="left" size="small"
-                        onClick={this.changeMenuVisible.bind(this)}
+                    <Icon className="fs-visible-operation-button"
+                        type={`${this.state.menuExpand ? 'menu-fold' : 'menu-unfold'}`}
+                        title={this.state.menuExpand ? lang('折叠菜单', 'Fold Menu') : lang('展开菜单', 'Expand Menu')}
+                        onClick={this.changeMenuExpand.bind(this)}
                     />
                 </div>
-                <Menu
+                <Menu inlineIndent={16}
                     selectedKeys={[this.props.activePage]}
                     openKeys={this.props.activeMenu}
                     mode="inline"
                     onClick={this.forwardPage.bind(this)}
                     onOpenChange={this.openMenu.bind(this)}
                 >
-                    <Menu.Item key={routerPath.Dashboard}><Icon type="dashboard" />{lang('仪表盘', 'Dashboard')}</Menu.Item>
-                    <Menu.Item key={routerPath.MetadataNodes}><Icon type="file-text" />{lang('元数据节点', 'Metadata Nodes')}</Menu.Item>
-                    <Menu.Item key={routerPath.StorageNodes}><Icon type="hdd" />{lang('存储节点', 'Storage Nodes')}</Menu.Item>
-                    <Menu.Item key={routerPath.ClientStatistics}><Icon type="line-chart" />{lang('客户端统计', 'Client Statistics')}</Menu.Item>
-                    <Menu.Item key={routerPath.UserStatistics}><Icon type="dot-chart" />{lang('用户统计信息', 'User Statistics')}</Menu.Item>
+                    <Menu.Item key={routerPath.Dashboard}>
+                        <Icon type="dashboard" />{lang('仪表盘', 'Dashboard')}
+                    </Menu.Item>
+                    <Menu.Item key={routerPath.MetadataNodes}>
+                        <Icon type="hdd" />{lang('元数据节点', 'Metadata Nodes')}
+                    </Menu.Item>
+                    <Menu.Item key={routerPath.StorageNodes}>
+                        <Icon type="database" />{lang('存储节点', 'Storage Nodes')}
+                    </Menu.Item>
+                    <Menu.Item key={routerPath.ClientStatistics}>
+                        <Icon type="line-chart" />{lang('客户端统计', 'Client Statistics')}
+                    </Menu.Item>
+                    <Menu.Item key={routerPath.UserStatistics}>
+                        <Icon type="bar-chart" />{lang('用户统计', 'User Statistics')}
+                    </Menu.Item>
                     <Menu.SubMenu key="Management"
-                        title={<span><Icon type="appstore-o" /><span>{lang('管理', 'Management')}</span></span>}
+                        title={<span><Icon type="tool" /><span>{lang('管理', 'Management')}</span></span>}
                     >
-                        <Menu.Item key={routerPath.ManagementKnownProblems}>{lang('已知问题', 'Known Problems')}</Menu.Item>
-                        <Menu.Item key={routerPath.ManagementLogFile}>{lang('日志文件', 'Log File')}</Menu.Item>
+                        <Menu.Item key={routerPath.ManagementKnownProblems}>
+                            <Icon type="frown-o" />{lang('已知问题', 'Known Problems')}
+                        </Menu.Item>
+                        <Menu.Item key={routerPath.ManagementLogFile}>
+                            <Icon type="file-text" />{lang('日志文件', 'Log File')}
+                        </Menu.Item>
                     </Menu.SubMenu>
-                    <Menu.SubMenu key="FSOperation"
-                        title={<span><Icon type="setting" /><span>{lang('文件系统操作', 'FS Operation')}</span></span>}
+                    <Menu.SubMenu key="FSOperation" inlineIndent={16}
+                        title={<span><Icon type="switcher" /><span>{lang('文件系统操作', 'FS Operation')}</span></span>}
                     >
-                        <Menu.Item key={routerPath.FSOperationStripeSettings}>{lang('条带设置', 'Stripe Settings')}</Menu.Item>
-                        <Menu.Item key={routerPath.FSOperationFileBrowser}>{lang('文件浏览器', 'File Browser')}</Menu.Item>
+                        <Menu.Item key={routerPath.FSOperationStripeSettings}>
+                            <Icon type="setting" />{lang('条带设置', 'Stripe Settings')}
+                        </Menu.Item>
+                        <Menu.Item key={routerPath.FSOperationFileBrowser}>
+                            <Icon type="folder-open" />{lang('文件浏览器', 'File Browser')}
+                        </Menu.Item>
                     </Menu.SubMenu>
                 </Menu>
             </aside>
