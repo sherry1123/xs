f3eadec7aa4f65ac491b092bce760679a9fbe2fb
向阳 <suiyuran@163.com>
2018-03-08 20:25:06 +0800

    modify router for pkg

--- server/router/index.js
+++ server/router/index.js
@@ -1,4 +1,5 @@
-const router = new require('koa-router')();
+const Router = require('koa-router');
+const router = new Router();
 const controller = require('../controller');
 
 router.all('/api/testapi', controller['/api/testapi']);
