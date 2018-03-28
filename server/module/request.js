084d95f05ecd83dd6060a545f967909b74ad6ccc
向阳 <suiyuran@163.com>
2018-03-28 19:29:43 +0800

    add disk list api

--- server/module/request.js
+++ server/module/request.js
@@ -1,30 +1,30 @@
 const request = require('request');
 const querystring = require('querystring');
 const initSearchUrl = (url, param) => (param ? url + '?' + querystring.stringify(param) : url);
-exports.get = (url, param, token={}, isJson) => {
+exports.get = (url, param, token = {}, isJson) => {
     return new Promise((resolve, reject) => {
-        request.get(initSearchUrl(url, param), {headers: token}, (error, response, body) => {
-            !error && response.statusCode === 200 ? resolve(isJson ? JSON.parse(body) : body) : reject(error); 
+        request.get(initSearchUrl(url, param), { headers: token }, (error, response, body) => {
+            !error && (response.statusCode === 200 || response.statusCode === 201) ? resolve(isJson ? JSON.parse(body) : body) : reject(error);
         });
     });
 };
-exports.post = (url, param, token={}, isJson) => {
+exports.post = (url, param, token = {}, isJson) => {
     return new Promise((resolve, reject) => {
-        request.post(url,{headers: token, form: param}, (error, response, body) => {
-            !error && response.statusCode === 200 ? resolve(isJson ? JSON.parse(body) : body) : reject(error); 
-        });
-    });
-};
-exports.put = (url, param, token={}, isJson) => {
-    return new Promise((resolve, reject) => {
-        request.put(url, {headers: token, form: param}, (error, response, body) => {
+        request.post(url, { headers: token, form: param }, (error, response, body) => {
             !error && response.statusCode === 200 ? resolve(isJson ? JSON.parse(body) : body) : reject(error);
         });
     });
 };
-exports.delete = (url, param, token={}, isJson) => {
+exports.put = (url, param, token = {}, isJson) => {
     return new Promise((resolve, reject) => {
-        request.del(url, {headers: token, form: param}, (error, response, body) => {
+        request.put(url, { headers: token, form: param }, (error, response, body) => {
+            !error && response.statusCode === 200 ? resolve(isJson ? JSON.parse(body) : body) : reject(error);
+        });
+    });
+};
+exports.delete = (url, param, token = {}, isJson) => {
+    return new Promise((resolve, reject) => {
+        request.del(url, { headers: token, form: param }, (error, response, body) => {
             !error && response.statusCode === 200 ? resolve(isJson ? JSON.parse(body) : body) : reject(error);
         });
     });
