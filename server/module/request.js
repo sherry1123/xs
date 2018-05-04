9516daef831cb32159a4438c643a04d8740433c5
向阳 <suiyuran@163.com>
2018-05-04 11:01:53 +0800

    modify check cluster env

--- server/module/request.js
+++ server/module/request.js
@@ -2,28 +2,28 @@ const request = require('request');
 const handler = require('./handler');
 const querystring = require('querystring');
 const initSearchUrl = (url, param) => (handler.emptyObject(param) ? url : url + '?' + querystring.stringify(param));
-exports.get = (url, param, token = {}, isJson) => {
+exports.get = (url, param, token, isJson, mockData) => {
     return new Promise((resolve, reject) => {
         request.get(initSearchUrl(url, param), { headers: token }, (error, response, body) => {
-            !error && response.statusCode === 200 ? resolve(isJson ? JSON.parse(body) : body) : reject(error);
+            !error && response.statusCode === 200 ? resolve(isJson ? JSON.parse(body) : body) : mockData ? resolve(mockData) : reject(error);
         });
     });
 };
-exports.post = (url, param, token = {}, isJson) => {
+exports.post = (url, param, token, isJson) => {
     return new Promise((resolve, reject) => {
         request.post(url, { headers: token, json: isJson, body: param }, (error, response, body) => {
             !error && response.statusCode === 200 ? resolve(body) : reject(error);
         });
     });
 };
-exports.put = (url, param, token = {}, isJson) => {
+exports.put = (url, param, token, isJson) => {
     return new Promise((resolve, reject) => {
         request.put(url, { headers: token, json: isJson, body: param }, (error, response, body) => {
             !error && response.statusCode === 200 ? resolve(body) : reject(error);
         });
     });
 };
-exports.delete = (url, param, token = {}, isJson) => {
+exports.delete = (url, param, token, isJson) => {
     return new Promise((resolve, reject) => {
         request.del(url, { headers: token, json: isJson, body: param }, (error, response, body) => {
             !error && response.statusCode === 200 ? resolve(body) : reject(error);
