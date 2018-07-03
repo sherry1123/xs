34720c2b5a4eab277b1c41d984362c710a5c1908
liuxiaoyu <461565610@qq.com>
2018-07-03 11:51:56 +0800

    Some code.

--- src/views/asyncLoad.js
+++ src/views/asyncLoad.js
@@ -24,9 +24,9 @@ export default importComponentCallBack =>  {
     };
 };
 
-// This is a HOC factory which is used as a function to collaborate with webpack Code-Splitting feature.
+// This is a HOC factory which is used as a component creation function to collaborate with webpack Code-Splitting feature.
 // It receives a component dynamically import callback function, and return the HOC(AsyncComponent).
-// The HOC will load the target component(a js extension file named like *.chunk.js) synchronously
-// after itself is mounted, and render the then loaded component with props proxy.
+// The HOC will load the target component(a js extension file named like *.chunk.js) synchronously after itself is mounted,
+// and render the then loaded component with props proxy.
 // Through these work, the loading performance of first screen of OrcaFs-UI web App will be greatly improved!
 // HOC here means High Order Component, it's a designing pattern in React, also a basic concept in functional programming.
