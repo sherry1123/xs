78f14a69d6a3e59e2d175ff2d8eb5056e7d60e12
xiaoyu.liu <password>
2018-10-11 15:40:11 +0800

    Add some size unit.

--- src/services/localData/index.js
+++ src/services/localData/index.js
@@ -1,7 +1,9 @@
 export const capacityUnitSize = {
+    'PB': 1024 * 1024 * 1024 * 1024 * 1024,
     'TB': 1024 * 1024 * 1024 * 1024,
     'GB': 1024 * 1024 * 1024,
-    'MB': 1024 * 1024
+    'MB': 1024 * 1024,
+    'KB': 1024
 };
 
 export const timeUnitMilliSecond = {
